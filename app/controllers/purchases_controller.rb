1e2a7c7e3b9daaabeb420a45d198105dadfc5e28
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-10-21 17:59:08 +0300

    Destroy purchases.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,7 +9,14 @@ class PurchasesController < ApplicationController
       flash.now[:notice] = t('application.flash_created')
     else
       flash.now[:alert] = t('application.error_created')
+      render partial: "form", locals: { product: Cost.find(resource_params[:cost_id]).purchases.build, cost_id: resource_params[:cost_id] }
     end
+  end
+
+  def destroy
+    @purchase = Purchase.find(params[:id])
+    @purchase.destroy
+    flash.now[:notice] = t('applicationpurchase_destroyed')
   end
 
   private
