d2f5c68a28b3f272cfecc3ac66f492a31e8a36e7
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-10-26 16:15:01 +0300

    Create report

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :costs, only: %i(index show)
   resources :purchases, only: %i(new create destroy)
   resources :incomes
+  resources :reports, only: %i(index)
 end
