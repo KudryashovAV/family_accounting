2d4297321fff28d92f50a034ec95b4dd609b0301
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-10-27 13:29:27 +0300

    Create report builder

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,7 @@
 class ReportsController < ApplicationController
 
   def index
-    if params[:report]
-      start_of_period = params[:report][:start].split('/')
-      end_of_period = params[:report][:end].split('/')
-      start_date = Date.new(start_of_period[2].to_i, start_of_period[1].to_i, start_of_period[0].to_i)
-      end_date = Date.new(end_of_period[2].to_i, end_of_period[1].to_i, end_of_period[0].to_i)
-      @incomes = Income.where('reporting_period >= ? AND reporting_period <= ?', start_date, end_date)
-      @costs = Cost.where('created_at >= ? AND created_at <= ?', start_date, end_date)
-    end
+    @report = ReportBuilder.build_report(params[:report]) if params[:report]
   end
 
 
