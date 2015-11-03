41527ee7f9d63cdb947d951e2edb827a2bba2808
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-11-03 10:38:32 +0300

    Cretae report builder

--- app/models/report_builder.rb
+++ app/models/report_builder.rb
@@ -1,13 +1,15 @@
 class ReportBuilder
 
-  def build_report(params)
-    if params[:report]
-      start_of_period = params[:report][:start].split('/')
-      end_of_period = params[:report][:end].split('/')
-      start_date = Date.new(start_of_period[2].to_i, start_of_period[1].to_i, start_of_period[0].to_i)
-      end_date = Date.new(end_of_period[2].to_i, end_of_period[1].to_i, end_of_period[0].to_i)
-      @incomes = Income.where('reporting_period >= ? AND reporting_period <= ?', start_date, end_date)
-      @costs = Cost.where('created_at >= ? AND created_at <= ?', start_date, end_date)
-    end
+  def self.build_report(params)
+    report = {}
+
+    start_of_period = params[:start].split('/')
+    end_of_period = params[:end].split('/')
+    start_date = Date.new(start_of_period[2].to_i, start_of_period[1].to_i, start_of_period[0].to_i)
+    end_date = Date.new(end_of_period[2].to_i, end_of_period[1].to_i, end_of_period[0].to_i)
+    report[:incomes] = Income.where('reporting_period >= ? AND reporting_period <= ?', start_date, end_date)
+    report[:costs] = Cost.where('created_at >= ? AND created_at <= ?', start_date, end_date)
+
+    report
   end
 end
