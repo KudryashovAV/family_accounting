d25346ed78ea99f1feba9e1e9bdba1e3a1286280
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-11-13 18:57:40 +0300

    Add css

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -1,6 +1,8 @@
 class CostsController < ApplicationController
   def index
-    @costs = group_by_period(Cost.all, :created_at, '%B %Y')
+    all_costs = group_by_period(Cost.all, :created_at, '%B %Y')
+    @current_period_costs = all_costs.delete(I18n.l(Date.today, format: '%B %Y'))
+    @costs = all_costs
   end
 
   def show
